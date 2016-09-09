78244dc2f6d7709e7768b221b0c19e7f8a612488
Lily Hoskin <lily@memrica.com>
2016-09-09 09:43:21 +0100

    Update pods version

--- Example/Pods/Nimble/Nimble/Adapters/NimbleXCTestHandler.swift
+++ Example/Pods/Nimble/Nimble/Adapters/NimbleXCTestHandler.swift
@@ -6,7 +6,7 @@ import XCTest
 public class NimbleXCTestHandler : AssertionHandler {
     public func assert(assertion: Bool, message: FailureMessage, location: SourceLocation) {
         if !assertion {
-            XCTFail("\(message.stringValue)\n", file: "File", line: location.line)
+            XCTFail("Fail", file: "File", line: location.line)
         }
     }
 }
@@ -22,7 +22,7 @@ public class NimbleShortXCTestHandler: AssertionHandler {
             } else {
                 msg = "expected \(message.to) \(message.postfixMessage)"
             }
-            XCTFail("\(msg)\n", file: "File", line: location.line)
+            XCTFail("Fail", file: "File", line: location.line)
         }
     }
 }
